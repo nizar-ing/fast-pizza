eb1c9a9a22f037def3a44d3aa6ea5e05a3c29e8c
nizarIng <nizar.ilahi@gmail.com>
2024-03-24 23:13:42 +0100

    Styling Text.

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="text-sm font-semibold">I. NIZAR</div>;
+}
+
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder.jsx";
+import Username from "../features/user/Username.jsx";
 
 export function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link className="tracking-widest" to="/">
+        Fast Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>I. Nizar</p>
+      <Username />
     </header>
   );
 }
