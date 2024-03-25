83f626c094b11896593ea02eaf72b4ed5889da4f
nizarIng <nizar.ilahi@gmail.com>
2024-03-25 15:56:35 +0100

    Applying CSS Grid.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,19 +1,21 @@
-import {Header} from "./Header.jsx";
+import { Header } from "./Header.jsx";
 import CartOverview from "../features/cart/CartOverview.jsx";
-import {Outlet, useNavigation} from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Loader from "./Loader.jsx";
 
 export function AppLayout() {
-    const navigation = useNavigation();
-    const isLoading = navigation.state === 'loading';
-    return (
-        <div className='layout'>
-            {isLoading && <Loader />}
-            <Header/>
-            <main>
-                <Outlet/>
-            </main>
-            <CartOverview/>
-        </div>
-    )
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+      <Header />
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+      <CartOverview />
+    </div>
+  );
 }
