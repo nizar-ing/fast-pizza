cfb31efe1aa031daee3e2826b6257bb5b3577229
nizarIng <nizar.ilahi@gmail.com>
2024-03-25 14:44:02 +0100

    Using flexbox as a layout manager.

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">I. NIZAR</div>;
+  return <div className="hidden text-sm font-semibold md:block">I. NIZAR</div>;
 }
 
 export default Username;
