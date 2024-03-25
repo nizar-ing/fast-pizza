8cfe66c73bc89a3617d26e592b0ca5a9cd5edd80
nizarIng <nizar.ilahi@gmail.com>
2024-03-25 18:02:16 +0100

    Reusing styles with @apply directive.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -48,25 +48,19 @@ function CreateOrder() {
         {/* implicitly => action='/order/new' ---> that means the route that provide this current component */}
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
         <div>
           <label>Address</label>
           <div>
-            <input
-              type="text"
-              name="address"
-              required
-              className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all placeholder:text-stone-400
-               focus:outline-none focus:ring focus:ring-yellow-500 md:px-6 md:py-3"
-            />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
         <div>
